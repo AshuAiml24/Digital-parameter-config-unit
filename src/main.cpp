be6b6369d61fcf652e137d99b2a62b5966f65352
Ashu1792 <ashupal112200@gmail.com>
2026-02-11 13:58:15 +0530

    add Screen width,screen height

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,10 @@
 #include <Adafruit_SSD1306.h>
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
+// OLED I2C address (most common is 0x3C)
+#define OLED_ADDR 0x3C
+// Create display object
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &amp;Wire, -1);
 void setup() {
 // write your initialization code here
 }
