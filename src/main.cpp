380859e05f0b9e3ed044d2ef5530b7d09a984ce8
Ashu1792 <ashupal112200@gmail.com>
2026-02-11 13:55:10 +0530

    Import libraries wire.h,Adafruit_GFX.h,Adafruit_SSD1306.h.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
 #include <Arduino.h>
+#include <wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
 void setup() {
 // write your initialization code here
 }
